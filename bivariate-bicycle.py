6bf99ca5e9cb826f4e94652b879b7612befd0bdf
davisclarke <clarked126@gmail.com>
2024-03-21 18:08:20 -0400

    init commit

---
+++ bivariate-bicycle.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import rustworkx as rx
+
+class BivariateBicylce():
+    def __init__(self, l, m) -> None: 
+        self.l=l
+        self.m=m
+        self.n=2*l*m
+
+class SurfaceCode():
+    def __init__(self) -> None:
+        pass
+
